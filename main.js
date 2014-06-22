07c302f9637041669fcff48bab50393cd5ab027e
John Regner <john@johnregner.com>
2014-06-22 11:37:06 -0500

    Initial Commit.

---
+++ main.js
@@ -0,0 +1,65 @@
+function getTime(){
+  var today = new Date();
+	var hours = today.getHours();
+	var minutes = today.getMinutes();
+  var seconds = today.getSeconds();
+  
+  hours   = (hours >= 10)   ? String(hours)   : "0" + String(hours);
+	minutes = (minutes >= 10) ? String(minutes) : "0" + String(minutes);
+	seconds = (seconds >= 10) ? String(seconds) : "0" + String(seconds);
+	return hours + minutes + seconds;
+}
+
+function lengthTwo(inputHex){
+  if (inputHex.length != 2 ){
+    return "0" + inputHex;
+  }
+  else {
+    return inputHex;
+  }
+}
+
+function normalizeTime(timestring){
+  var redcolor = Number(timestring.substring(0,2));
+  var greencolor = Number(timestring.substring(2,4));
+  var bluecolor = Number(timestring.substring(4,6));
+  //Since the time ranges are 0-24, 0-60, 0-60 lets normalizze them to fill the color range 0-255
+  var redHex   = Math.round( redcolor   * (255/24)).toString(16);
+  var greenHex = Math.round( greencolor * (255/60)).toString(16);
+  var blueHex  = Math.round( bluecolor  * (255/60)).toString(16);
+
+  // Ensure always 2 digits
+  redHex   = lengthTwo(redHex);
+  greenHex = lengthTwo(greenHex);
+  blueHex  = lengthTwo(blueHex);
+  return  redHex + greenHex + blueHex; 
+}
+
+function getCompliment(inputHex){
+  var redComp   = (255 - Number( '0x' + inputHex.substring(0,2))).toString(16);
+  var greenComp = (255 - Number( '0x' + inputHex.substring(2,4))).toString(16);
+  var blueComp  = (255 - Number( '0x' + inputHex.substring(4,6))).toString(16);
+  return redComp + greenComp + blueComp;
+}
+
+function timeout() {
+	window.setInterval(function() {
+
+	  var myNumberElements = document.getElementsByClassName('number');
+	  var timestring = getTime();
+    document.getElementsByClassName("number")[0].innerHTML = timestring;
+    
+    var newBackground = normalizeTime(timestring);
+    document.getElementsByTagName("body")[0].style.backgroundColor = '#' + newBackground;
+
+    //set number color to be the compliment of the background
+    var numberColor = getCompliment(newBackground);
+    document.getElementsByClassName("number")[0].style.color = "#" + numberColor;
+
+    //display the actual used colors
+    document.getElementsByClassName("bg")[0].innerHTML = "Background  #" + newBackground;
+    document.getElementsByClassName("fg")[0].innerHTML = "Numbers  #" + numberColor;
+  }, 1000);
+}
+
+timeout();
